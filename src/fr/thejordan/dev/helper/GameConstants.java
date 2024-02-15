688e654eb2e005d56fe9e59af6b5619a722e2d14
Jordan <thejordan.dev@gmail.com>
2024-02-15 13:19:28 +0100

    Modification messages

--- src/fr/thejordan/dev/helper/GameConstants.java
+++ src/fr/thejordan/dev/helper/GameConstants.java
@@ -8,8 +8,8 @@ public class GameConstants {
 	public static final String GAME_TIMER_LABEL(String time) { return "Timer: "+time;}
 	public static final String GAME_TRIES_LABEL(int tries) { return "Essais: "+tries;}
 
-	public static final String GAME_OVER_FINISHED_MESSAGE(String time) { return "Bravo ! Vous avez terminé en "+time+" !"; };
-	public static final String GAME_OVER_TIME_UP_MESSAGE = "Temps écoulé ! Recommencer ?";
-	public static final String GAME_OVER_NO_MORE_TRIES_MESSAGE = "Plus d'essais ! Recommencer ?";
+	public static final String GAME_OVER_FINISHED_MESSAGE(String time) { return "Bravo ! Vous avez terminé en "+time+" !\nRecommencer ?"; };
+	public static final String GAME_OVER_TIME_UP_MESSAGE = "Temps écoulé !\nRecommencer ?";
+	public static final String GAME_OVER_NO_MORE_TRIES_MESSAGE = "Plus d'essais !\nRecommencer ?";
 
 }
